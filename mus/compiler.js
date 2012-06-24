e66cf8eef15024b08a5cac1b1dd692651c67b67a
vandeput <irvan.putra@gmail.com>
2012-06-24 11:38:26 +0700

    Add 'repeat' tag
    Signed-off-by: vandeput <irvan.putra@gmail.com>

--- mus/compiler.js
+++ mus/compiler.js
@@ -1,30 +1,25 @@
 // this is all you
 var endTime = function (time, expr) {
     // your code here
-    if (expr.tag === 'note') {
-        return time + expr.dur;
-    }
-    else if (expr.tag === 'seq') {
-        return endTime(endTime(time, expr.left),expr.right);
-    }
-    else if (expr.tag === 'par') {
-        return Math.max(endTime(time, expr.left),endTime(time,expr.right));
-    }
-    else if (expr.tag === 'rest') {
-        return time + expr.duration;
-    }
+	switch (expr.tag) {
+		case 'note': return time + expr.dur; break;
+		case 'seq': return time + endTime(endTime(0, expr.left),expr.right); break;
+		case 'par': return time + Math.max(endTime(0, expr.left),endTime(0,expr.right)); break;
+		case 'rest': return time + expr.dur; break;
+		case 'repeat': return time + endTime(0, expr.section)*expr.count; break;
+	}
 };
 
 // maybe some helper functions
 var letterPitch = function (letter) {
-	switch(letter) {
-		case 'c': return 0;
-		case 'd': return 2;
-		case 'e': return 4;
-		case 'f': return 5;
-		case 'g': return 7;
-		case 'a': return 9;
-		case 'b': return 11;
+	switch (letter) {
+		case 'c': return 0; break;
+		case 'd': return 2; break;
+		case 'e': return 4; break;
+		case 'f': return 5; break;
+		case 'g': return 7; break;
+		case 'a': return 9; break;
+		case 'b': return 11; break;
 	}
 };
 
@@ -33,28 +28,34 @@ var convertPitch = function(pitch) {
 };
 
 var compileWithTime = function (musexpr, time) {
-    if (musexpr.tag === 'note') {
-        return [{
-            tag: musexpr.tag,
-            pitch: convertPitch(musexpr.pitch),
-            start: time,
-            dur: musexpr.dur
-        }];
-    }
-    else if (musexpr.tag === 'seq') {
-        return compileWithTime(musexpr.left,time).concat(compileWithTime(musexpr.right,endTime(time,musexpr.left)));
-    }
-    else if (musexpr.tag === 'par') {
-        return compileWithTime(musexpr.left,time).concat(compileWithTime(musexpr.right,time));
-    }
-    else if (musexpr.tag === 'rest') {
-        return [{
-            tag: musexpr.tag,
-            pitch: '',
-            start: time,
-            dur: musexpr.dur
-        }];
-    }
+	var returnArray = [];
+	switch (musexpr.tag) {
+		case 'note':
+			returnArray = [{
+				tag: musexpr.tag,
+				pitch: convertPitch(musexpr.pitch),
+				start: time,
+				dur: musexpr.dur
+			}];
+			break;
+		case 'seq':
+			returnArray = compileWithTime(musexpr.left,time).concat(compileWithTime(musexpr.right,endTime(time,musexpr.left)));
+			break;
+		case 'par':
+			returnArray = compileWithTime(musexpr.left,time).concat(compileWithTime(musexpr.right,time));
+			break;
+		case 'rest':
+	        returnArray = [];
+			break;
+		case 'repeat':
+			var currentEndTime = time;
+			for (i=1; i<=musexpr.count; i++) {
+				returnArray = returnArray.concat(compileWithTime(musexpr.section, currentEndTime));
+				currentEndTime = endTime(currentEndTime, musexpr.section);
+			}
+			break;
+	}
+	return returnArray;
 };
 
 var compile = function (musexpr) {
@@ -63,15 +64,30 @@ var compile = function (musexpr) {
 };
 
 var melody_mus = 
-    { tag: 'seq',
-      left: 
-       { tag: 'seq',
-         left: { tag: 'note', pitch: 'a4', dur: 250 },
-         right: { tag: 'note', pitch: 'b4', dur: 250 } },
-      right:
-       { tag: 'seq',
-         left: { tag: 'note', pitch: 'c4', dur: 500 },
-         right: { tag: 'note', pitch: 'd4', dur: 500 } } };
+        { tag: 'seq',
+          left: 
+           { tag: 'seq',
+             left: { tag: 'note', pitch: 'a4', dur: 250 },
+             right: {
+               tag : 'par',
+               left : { tag: 'note', pitch: 'b4', dur: 250 },
+               right : {
+                 tag : 'seq',
+                  left : {tag : 'note', pitch : 'g5', dur : 100},
+                  right : {tag : 'rest', dur : 300}
+               }
+              }
+          },
+          right:
+           { tag: 'seq',
+             left: { tag: 'repeat',
+               section: { 
+                 tag : 'seq', 
+                 left : {tag: 'note', pitch: 'c4', dur: 250 },
+                 right : { tag: 'rest',  dur: 500 }
+                 }, 
+               count: 3 },
+             right: { tag: 'note', pitch: 'd4', dur: 500 } } };
 
 console.log(melody_mus);
 console.log(compile(melody_mus));
